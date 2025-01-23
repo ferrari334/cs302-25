c3890d1620ad3015dcbb115eff1ac8f2bde4369e
Matthew Mark Ferrari <dwp393@hydra11.eecs.utk.edu>
2025-01-23 13:19:36 -0500

    added correct solution.cpp, g++ errors.g++: error: test-output: No such file or directory g++: error: test-memory: No such file or directory make: *** [<builtin>: test] Error 1

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -1,6 +1,55 @@
+// Matthew Ferrari
 // Challenge 01: Rotating Arrays; null solution
-// git@github.com:ferrari334/cs302-25.git
+// this takes in 3 commands length, rotations and direction and rotates length
+// acordingly.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 int main(int argc, char *argv[]) {
+
+  // take the input 2 and subtract by input 1 if the 3rd is left and add if 3rd
+  // is R
+  int length, rotations;
+  char direction;
+  // reading in information about the rotation
+  while (cin >> length >> rotations >> direction) {
+
+    // vector to place elements that are read in
+    vector<int> order(length);
+
+    // read in elements
+    for (int i = 0; i < length; i++) {
+      cin >> order[i];
+    }
+
+    // check that rotation doesn't repeat starting order
+    rotations %= length;
+
+    // left rotation
+    if (direction == 'L') {
+      // add elements to a vector
+      for (int i = 0; i < length; i++) {
+        cout << order[(i + rotations) % length];
+        if (i < length - 1)
+          cout << " ";
+      }
+    }
+
+    // right rotation
+    else {
+      for (int i = 0; i < length; i++) {
+        cout << order[(i + (length - rotations)) % length];
+        if (i < length - 1)
+          cout << " ";
+      }
+    }
+    cout << "\n";
+  }
+  // if 2nd is greater than 1st input then subtract and move left that many
+  // values
+
   return 0;
-  char;
 }
